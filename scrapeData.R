c74ef2bcf0026e14675302f476aed99fc6f55e2a
Pranav Singh <pranavsingh92@gmail.com>
2017-04-05 02:54:21 -0400

    scrape Woolridge data also. TODO: abstract out scraping code

--- scrapeData.R
+++ scrapeData.R
@@ -14,3 +14,17 @@ for (dn in data_names) {
     download.file(data_links[dn], destfile)
   }
 }
+
+data_dir_html <- 'http://evansresearch.us/DSC/Spring2017/ECMT/Data_Woolridge/'
+data_names <- read_html(data_dir_html) %>% html_nodes("a") %>% html_attr('href') %>% str_subset('.csv|.xls|.xlsx$')
+data_links <- paste0(data_dir_html, data_names)
+names(data_links) <- data_names
+# m <- regexpr('(?<=/)[^/]*$', data_links, perl = T)
+# regmatches(data_links, m)
+for (dn in data_names) {
+  destfile <- paste0('./Data_Woolridge/', dn)
+  if (!file.exists(destfile)) {
+    # browser()
+    download.file(data_links[dn], destfile)
+  }
+}
